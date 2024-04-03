b5ab648518d90fb8ffe4471e1cf973a448af6044
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2024-04-03 16:57:42 +0600

    Add static address

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.6.1")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.6.2")),
         .package(url: "https://github.com/greymass/swift-scrypt.git", from: "1.0.0"),
         .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.5")),
     ],
